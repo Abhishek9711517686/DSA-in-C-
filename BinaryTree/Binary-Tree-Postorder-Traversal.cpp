66cca42b5a633d93691050268a8c43b86dca0a61
Abhishek Kumar <richabhikumaryadav21@gmail.com>
2025-09-28 19:44:35 +0530

    [145] [Time Beats: 100.00%] [Memory Beats: 71.75%] - LeetPush

--- BinaryTree/Binary-Tree-Postorder-Traversal.cpp
+++ BinaryTree/Binary-Tree-Postorder-Traversal.cpp
@@ -9,28 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
         vector<int> postorder;
-        if(root == NULL) return postorder;
-        stack<TreeNode*> st1, st2;
-        st1.push(root);
-        while(!st1.empty()){
-            root = st1.top();
-            st1.pop();
-            st2.push(root);
-            if(root->left != NULL){
-                st1.push(root->left);
+        if (root == nullptr) return postorder;
+
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        TreeNode* temp = nullptr;
+
+        while (cur != nullptr || !st.empty()) {
+            if (cur != nullptr) {
+                st.push(cur);
+                cur = cur->left;
+            } else {
+                temp = st.top()->right;
+                if (temp == nullptr) {
+                    temp = st.top();
+                    st.pop();
+                    postorder.push_back(temp->val);
+                    while (!st.empty() && temp == st.top()->right) {
+                        temp = st.top();
+                        st.pop();
+                        postorder.push_back(temp->val);
+                    }
+                } else {
+                    cur = temp;
+                }
             }
-            if(root->right != NULL){
-                st1.push(root->right);
-            }            
         }
-        while(!st2.empty()){
-            postorder.push_back(st2.top()->val);
-            st2.pop();
-        } 
         return postorder;
     }
 };
