7079c766bdb8e550a5d66799c3045361716ac8e8
Abhishek Kumar <richabhikumaryadav21@gmail.com>
2025-11-14 15:49:09 +0530

    [3] [Time Beats: 82.26%] [Memory Beats: 67.19%] - LeetPush

--- TwoPointer&SlidingWindow/Longest-Substring-Without-Repeating-Characters.cpp
+++ TwoPointer&SlidingWindow/Longest-Substring-Without-Repeating-Characters.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int n = s.size();
-        int maxlen = 0;
-        for(int i = 0; i < n; i++){
-            int hash[256] = {0};
-            for(int j = i; j < n; j++){
-                if(hash[s[j]] == 1) break;
-                int len = j - i + 1;
-                maxlen = max(len, maxlen);
-                hash[s[j]] = 1;
+        int n = (int)s.size();
+        vector<int> hash(256, -1);
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+        while(r < n){
+            if(hash[s[r]] != -1){
+                if(hash[s[r]] >= l){
+                    l = hash[s[r]] + 1;
+                }
             }
+            int len = r - l + 1;
+            maxLen = max(len, maxLen);
+            hash[s[r]] = r;
+            r++;
         }
-        return maxlen;
+        return maxLen;
     }
 };
