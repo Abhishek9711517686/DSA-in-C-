00114b8aabc9035d14178af2280bdbceb266a20a
Abhishek Kumar <richabhikumaryadav21@gmail.com>
2025-11-20 10:18:37 +0530

    [424] [Time Beats: 100.00%] [Memory Beats: 69.39%] - LeetPush

---
+++ TwoPointer&SlidingWindow/Longest-Repeating-Character-Replacement.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        vector<int> freq(26,0);
+        int l = 0, maxFreq = 0, ans = 0;
+        for(int r = 0; r < n; r++){
+            freq[s[r] - 'A']++;
+            maxFreq = max(maxFreq, freq[s[r] -'A']);
+
+            while((r - l + 1) - maxFreq > k){
+                freq[s[l] - 'A']--;
+                l++;
+            }
+            ans = max(ans, r - l + 1);
+        }
+        return ans;
+    }
+};
